ddcd5071be48275210d52e65a2180938fb919428
c14190036 <100283424+c14190036@users.noreply.github.com>
2022-06-27 13:07:09 +0700

    update UI

--- fitathome/lib/main.dart
+++ fitathome/lib/main.dart
@@ -9,68 +9,59 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-   options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseAuth auth = FirebaseAuth.instance;
-  runApp(
-    MaterialApp(
-      title: "Fit At Home",
-      theme: ThemeData(
-      scaffoldBackgroundColor: Color(0xff001934),
-      textTheme: TextTheme(
-        headline1: TextStyle(color: Colors.white),
-        headline2: TextStyle(color: Colors.white),
-        bodyText2: TextStyle(color: Colors.white),
-      ),
-      iconTheme: IconThemeData(color: Colors.white),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          primary: Color(0xff6bc5c5),
+  runApp(MaterialApp(
+    title: "Fit At Home",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xff001934),
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.white),
+          headline2: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(color: Colors.white),
         ),
-        
-      ),
-      hintColor: Colors.white,
-      inputDecorationTheme: InputDecorationTheme(
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color(0xff6bc5c5))
+        iconTheme: IconThemeData(color: Colors.white),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xff6bc5c5),
+          ),
         ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color(0xff6bc5c5))
-        ),
-        labelStyle: TextStyle(color: Color(0xff6bc5c5)),
-        
-      )
-
-    ),
-      home: MyApp(),
-    )
-  );
+        hintColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xff6bc5c5))),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xff6bc5c5))),
+          labelStyle: TextStyle(color: Color(0xff6bc5c5)),
+        )),
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   final _controllerEmail = TextEditingController();
   final _controllerPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Home();
-          } else {
-            return Login();
-          }
-      })
-    );
+        body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Home();
+              } else {
+                return Login();
+              }
+            }));
   }
 }
--- fitathome/lib/register.dart
+++ fitathome/lib/register.dart
@@ -7,14 +7,13 @@ import 'package:fitathome/dcProfile.dart';
 import 'package:fitathome/main.dart';
 
 class Register extends StatefulWidget {
-  const Register({ Key? key }) : super(key: key);
+  const Register({Key? key}) : super(key: key);
 
   @override
   State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-
   final namaController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -50,22 +49,20 @@ class _RegisterState extends State<Register> {
             child: Column(
               children: [
                 Container(
-                  child: Image.asset('assets/logofitathomev2.png',height: 100)
-                ),
+                    child: Image.asset('assets/logo_v3.png', height: 150)),
                 Container(
-                  child: Text("Register", style: TextStyle(fontSize: 20),),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
                   child: TextFormField(
                     style: TextStyle(color: Colors.white),
                     controller: namaController,
                     keyboardType: TextInputType.name,
                     decoration: const InputDecoration(
-                      prefixIcon: const Icon(Icons.person, color: Color(0xff6bc5c5),),
-                      border: OutlineInputBorder(),
-                      labelText: "Nama"
-                    ),
+                        prefixIcon: const Icon(
+                          Icons.person,
+                          color: Color(0xff6bc5c5),
+                        ),
+                        border: OutlineInputBorder(),
+                        labelText: "Nama"),
                     validator: (value) {
                       if (value!.isEmpty) {
                         return ("Nama tidak boleh kosong!");
@@ -74,15 +71,17 @@ class _RegisterState extends State<Register> {
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
                   child: TextFormField(
                     style: TextStyle(color: Colors.white),
                     controller: emailController,
                     decoration: const InputDecoration(
-                      prefixIcon: const Icon(Icons.email_outlined, color: Color(0xff6bc5c5),),
-                      border: OutlineInputBorder(),
-                      labelText: "Email"
-                    ),
+                        prefixIcon: const Icon(
+                          Icons.email_outlined,
+                          color: Color(0xff6bc5c5),
+                        ),
+                        border: OutlineInputBorder(),
+                        labelText: "Email"),
                     validator: (value) {
                       if (value!.isEmpty) {
                         return ("Email tidak boleh kosong!");
@@ -91,79 +90,89 @@ class _RegisterState extends State<Register> {
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
                   child: TextFormField(
                     style: TextStyle(color: Colors.white),
                     controller: passwordController,
                     obscureText: passwordConfirmVisibility,
                     decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.vpn_key, color: Color(0xff6bc5c5),),
-                      border: OutlineInputBorder(),
-                      labelText: "Password",
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          passwordConfirmVisibility
-                          ? Icons.visibility_off
-                          : Icons.visibility,
+                        prefixIcon: const Icon(
+                          Icons.vpn_key,
                           color: Color(0xff6bc5c5),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            passwordConfirmVisibility = !passwordConfirmVisibility;
-                          });
-                        },
-                      )
-                    ),
+                        border: OutlineInputBorder(),
+                        labelText: "Password",
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            passwordConfirmVisibility
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Color(0xff6bc5c5),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              passwordConfirmVisibility =
+                                  !passwordConfirmVisibility;
+                            });
+                          },
+                        )),
                     validator: (value) {
                       if (value!.isEmpty) {
                         return ("Password tidak boleh kosong!");
                       }
                       if (value.length < 6) {
-                        return("min. character password adalah 6");
+                        return ("min. character password adalah 6");
                       }
                     },
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
                   child: TextFormField(
                     style: TextStyle(color: Colors.white),
                     controller: confirmpasswordController,
                     keyboardType: TextInputType.number,
                     obscureText: passwordVisibility,
                     decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.vpn_key, color: Color(0xff6bc5c5),),
-                      border: OutlineInputBorder(),
-                      labelText: "Confirm Password",
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          passwordVisibility
-                          ? Icons.visibility_off
-                          : Icons.visibility,
+                        prefixIcon: const Icon(
+                          Icons.vpn_key,
                           color: Color(0xff6bc5c5),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            passwordVisibility = !passwordVisibility;
-                          });
-                        },
-                      )
-                    ),
+                        border: OutlineInputBorder(),
+                        labelText: "Confirm Password",
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            passwordVisibility
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Color(0xff6bc5c5),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              passwordVisibility = !passwordVisibility;
+                            });
+                          },
+                        )),
                     validator: (value) {
                       if (value != passwordController.text) {
-                        return("Password tidak sama!");
+                        return ("Password tidak sama!");
                       }
                       if (value!.isEmpty) {
-                        return("Tidak boleh kosong!");
+                        return ("Tidak boleh kosong!");
                       }
                     },
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  width: double.infinity,
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
                   child: ElevatedButton(
-                    onPressed: () {register(emailController.text.toString(), passwordController.text.toString());},
-                    child: Text("REGISTER")),
+                      onPressed: () {
+                        register(emailController.text.toString(),
+                            passwordController.text.toString());
+                      },
+                      child: Text("REGISTER",
+                          style: TextStyle(color: Colors.black, fontSize: 16))),
                 )
               ],
             ),
@@ -174,15 +183,14 @@ class _RegisterState extends State<Register> {
   }
 
   void register(String email, String pass) async {
-    if(formKey.currentState!.validate()) {
-        await auth
-        .createUserWithEmailAndPassword(email: email, password: pass)
-        .then((value) => addData())
-        .catchError((e) {
-          SnackBar(content: Text("${e.message}"));
+    if (formKey.currentState!.validate()) {
+      await auth
+          .createUserWithEmailAndPassword(email: email, password: pass)
+          .then((value) => addData())
+          .catchError((e) {
+        SnackBar(content: Text("${e.message}"));
       });
     }
-    
   }
 
   addData() async {
@@ -194,12 +202,10 @@ class _RegisterState extends State<Register> {
     profile.email = user!.email;
     profile.nama = namaController.text;
 
-    await firestore
-      .collection("tbUser")
-      .doc(user.uid)
-      .set(profile.toJson());
+    await firestore.collection("tbUser").doc(user.uid).set(profile.toJson());
     SnackBar(content: Text("Akun telah dibuat"));
 
-    Navigator.pushAndRemoveUntil((context), MaterialPageRoute(builder: (context) => MyApp()), (route) => false);
+    Navigator.pushAndRemoveUntil((context),
+        MaterialPageRoute(builder: (context) => MyApp()), (route) => false);
   }
 }
